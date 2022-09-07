f02434e3cb7193ea4ed8c408830432be7b386d25
Tr4shL0rd <ketil04@gmail.com>
2022-09-07 10:26:49 +0200

    added flatten function

--- helper.py
+++ helper.py
@@ -1,13 +1,14 @@
 import subprocess
 
-
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+    
 def popUp(message: str, timeout=5, stockName="STOCKNAME"):
     try:
         subprocess.Popen(["notify-send", "-a", stockName,
                         "-t", str(timeout*1000), message])
     except FileNotFoundError:
         pass
-
 
 def clear(amount=32) -> str:
     print("\n"*amount)
