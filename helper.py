c8d5a1247e4eba38963453e60826e34c62c7988b
Tr4shL0rd <ketil04@gmail.com>
2022-09-07 10:57:25 +0200

    added comments

--- helper.py
+++ helper.py
@@ -1,14 +1,23 @@
 import subprocess
 
 def flatten(l):
+    '''
+        flattens and returns list l
+    '''
     return [item for sublist in l for item in sublist]
-    
+
 def popUp(message: str, timeout=5, stockName="STOCKNAME"):
+    '''
+        sends a KDE popup with a message, amount of time it is shown and the stockname 
+    '''
     try:
         subprocess.Popen(["notify-send", "-a", stockName,
                         "-t", str(timeout*1000), message])
     except FileNotFoundError:
         pass
 
-def clear(amount=32) -> str:
+def clear(amount=32):
+    '''
+        clears the screen with \n * amount
+    '''
     print("\n"*amount)
