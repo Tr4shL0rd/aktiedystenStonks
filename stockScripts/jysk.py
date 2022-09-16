9f579bfdeaf2d14a0d74a6e485a9d79f975d7dd6
Tr4shL0rd <ketil04@gmail.com>
2022-09-16 09:58:12 +0200

    better way of importing helper file

--- stockScripts/jysk.py
+++ stockScripts/jysk.py
@@ -3,9 +3,13 @@ import csv
 import json
 import time
 import requests
-import helper
 from sys import argv
 from rich import print as rprint
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import helper
 
 config = helper.loadJson()
 STOCKNAME="JYSK"
