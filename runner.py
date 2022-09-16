2903a443dbda25d09a2ffb69aa675248f0770f80
Tr4shL0rd <ketil04@gmail.com>
2022-09-16 09:57:33 +0200

    added HeartBeam Inc

--- runner.py
+++ runner.py
@@ -49,6 +49,7 @@ def stockPrice() -> list:
         "spot": "(NYSE~SPOT)",
         "oil":  "(BRENT~LCO)",
         "jysk": "(CPH~JYSK)",
+        "beat": "(NASDAQ~BEAT)",
     }
     # base url for all stock endpoints
     url = "https://aktiedysten.dk/z/chart?q=s.i1d.full"
@@ -57,20 +58,24 @@ def stockPrice() -> list:
     responseSpot = requests.get(f"{url}{urlIndex['spot']}")
     responseOil  = requests.get(f"{url}{urlIndex['oil']}")
     responseJysk = requests.get(f"{url}{urlIndex['jysk']}")
+    responseBeat = requests.get(f"{url}{urlIndex['beat']}")
     # data from each response 
     dataSpot = json.loads(responseSpot.text)
     dataOil  = json.loads(responseOil.text)
     dataJysk = json.loads(responseJysk.text)
+    dataBeat = json.loads(responseBeat.text)
     
     # price array from data (might be possible to do it in a smarter way)
     priceSpot = dataSpot["Encoded"]["Data"]
     priceOil  = dataOil["Encoded"]["Data"]
     priceJysk = dataJysk["Encoded"]["Data"]
+    priceBeat = dataBeat["Encoded"]["Data"]
     # individual prices from the price array given by data 
-    currentPriceSpot = round(priceSpot[-2], 1)
+    currentPriceSpot = round(priceSpot[-2],1)
     currentPriceOil  = round(priceOil[-1], 1)
     currentPriceJysk = round(priceJysk[-2],1)
-    return [currentPriceOil, currentPriceSpot, currentPriceJysk]
+    currentPriceBeat = round(priceBeat[-2],1)
+    return [currentPriceOil, currentPriceSpot, currentPriceJysk, currentPriceBeat]
 def main(load:bool=True):
     # checks wifi connection
     wifiCheck()
@@ -88,6 +93,7 @@ def main(load:bool=True):
         "spot": "NYSE",
         "oil": "BRENT",
         "jysk": "CPH",
+        "beat": "NASDAQ",
     }
     
     # a dict for the script name and description (description is not used anymore (find a way to remove))
@@ -96,6 +102,7 @@ def main(load:bool=True):
         "1": "oil.py" ,
         "2": "spot.py",
         "3": "jysk.py",
+        "4": "beat.py",
     }
     # letterScripts converts program name string into program index
     # program name to number
@@ -103,27 +110,32 @@ def main(load:bool=True):
         "oil":  "1",
         "spot": "2",
         "jysk": "3",
+        "beat": "4",
     }
     # opening and closing times for each market
     openTime = {
         "NYSE":  [(15,00), (22,00)],
         "CPH":   [(9 ,00), (17,00)],
         "BRENT": [(00,00), (23,00)],
+        "NASDAQ": [(15,00), (22,00)],
     }
     # prettier way of showing closing-opening times for the table 
     openTimePretty = {
         "NYSE":  "15:00 - 22:00",
         "CPH":   "09:00 - 17:00",
-        "BRENT": "00:00 - 00:00"
+        "BRENT": "00:00 - 00:00",
+        "NASDAQ": "15:00 - 22:00",
     }
     # fills the table
     if load:
+        helper.clear(1000)
         print("LOADING...")
         # formatted pricing 
         prices = {
             "oil":  f"${str(stockPrice()[0])}",
             "spot": f"${str(stockPrice()[1])}",
             "jysk": f"{str(stockPrice()[2])}DKK",
+            "beat": f"${str(stockPrice()[3])}",
         }
         for k,v in scripts.items():
             progName = v.split(".py")[0]
